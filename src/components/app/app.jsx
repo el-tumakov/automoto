1d91279ff7d1b2a9720aff4ffa31a981362ddf42
Егор Тумаков <el.tumakov@gmail.com>
2021-03-26 23:40:54 +0300

    Добавляет верстку шапки и подвала
    - создает стили для шрифтов
    - содает базовые стили, переменные и миксины
    - добавляет компоненты хедера и футера и стили для них

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import CarScreen from "../car-screen/car-screen";
+import "../../scss/style.scss";
 
 const App = () => (
   <BrowserRouter>
